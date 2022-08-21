64252f7bcdc694c342f874049df1297e4659baac
Batuhan Apaydın <batuhan.apaydin@trendyol.com>
2022-08-21 14:15:07 +0300

    feat: initial commit
    Signed-off-by: Batuhan Apaydın <batuhan.apaydin@trendyol.com>

---
+++ go.mod
@@ -0,0 +1,31 @@
+module github.com/developer-guy/mage-go-example
+
+go 1.18
+
+require (
+	github.com/carolynvs/magex v0.9.0
+	github.com/magefile/mage v1.13.0
+	github.com/spf13/cobra v1.5.0
+	sigs.k8s.io/release-utils v0.7.3
+)
+
+require (
+	github.com/Masterminds/semver/v3 v3.1.1 // indirect
+	github.com/andybalholm/brotli v1.0.1 // indirect
+	github.com/blang/semver/v4 v4.0.0 // indirect
+	github.com/common-nighthawk/go-figure v0.0.0-20210622060536-734e95fb86be // indirect
+	github.com/dsnet/compress v0.0.2-0.20210315054119-f66993602bf5 // indirect
+	github.com/golang/snappy v0.0.2 // indirect
+	github.com/inconshreveable/mousetrap v1.0.0 // indirect
+	github.com/klauspost/compress v1.11.4 // indirect
+	github.com/klauspost/pgzip v1.2.5 // indirect
+	github.com/mholt/archiver/v3 v3.5.1 // indirect
+	github.com/nwaples/rardecode v1.1.0 // indirect
+	github.com/pierrec/lz4/v4 v4.1.2 // indirect
+	github.com/sirupsen/logrus v1.9.0 // indirect
+	github.com/spf13/pflag v1.0.5 // indirect
+	github.com/ulikunitz/xz v0.5.9 // indirect
+	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
+	golang.org/x/sys v0.0.0-20220715151400-c0bba94af5f8 // indirect
+	k8s.io/utils v0.0.0-20220210201930-3a6ce19ff2f9 // indirect
+)
---
+++ magefile.go
@@ -0,0 +1,160 @@
+//go:build mage
+// +build mage
+
+package main
+
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+
+	"github.com/carolynvs/magex/pkg"
+	"github.com/carolynvs/magex/pkg/archive"
+	"github.com/carolynvs/magex/pkg/downloads"
+	"github.com/magefile/mage/sh"
+
+	"sigs.k8s.io/release-utils/mage"
+)
+
+// Default target to run when none is specified
+// If not set, running mage will list available targets
+var Default = BuildImagesLocal
+
+// BuildImages build bom image using ko
+func BuildImages() error {
+	fmt.Println("Building images with ko...")
+	if err := EnsureBinary("ko", "version", "0.11.2"); err != nil {
+		return err
+	}
+
+	ldFlags, _ := mage.GenerateLDFlags()
+	os.Setenv("LDFLAGS", ldFlags)
+	os.Setenv("KOCACHE", "/tmp/ko")
+
+	if os.Getenv("KO_DOCKER_REPO") == "" {
+		return errors.New("missing KO_DOCKER_REPO environment variable")
+	}
+	// echo "${{ github.token }}" | ./ko login ghcr.io --username "${{ github.actor }}" --password-stdin
+	_ = sh.RunV("ko", "login", os.Getenv("ghcr.io"), "-u", os.Getenv("GITHUB_ACTOR"), "-p", os.Getenv("GITHUB_TOKEN"))
+
+	return sh.RunV("ko", "build", "--bare",
+		"--platform=linux/amd64", "-t", "latest",
+		"-t", os.Getenv("GITHUB_REF_NAME"),
+		".")
+}
+
+// BuildImagesLocal build images locally and not push
+func BuildImagesLocal() error {
+	fmt.Println("Building images with ko for local...")
+	if err := EnsureBinary("ko", "version", "0.11.2"); err != nil {
+		return err
+	}
+
+	ldFlags, _ := mage.GenerateLDFlags()
+	os.Setenv("LDFLAGS", ldFlags)
+	os.Setenv("KOCACHE", "/tmp/ko")
+
+	return sh.RunV("ko", "build", "--bare",
+		"--local", "--platform=linux/amd64",
+		".")
+}
+
+func Release() error {
+	fmt.Println("Releasing greeting with goreleaser...")
+	if err := EnsureBinary("goreleaser", "-v", "1.10.3"); err != nil {
+		return err
+	}
+
+	ldFlags, _ := mage.GenerateLDFlags()
+	os.Setenv("LDFLAGS", ldFlags)
+
+	args := []string{"release", "--rm-dist"}
+
+	return sh.RunV("goreleaser", args...)
+}
+
+// Maybe we can  move this to release-utils
+func EnsureBinary(binary, cmd, version string) error {
+	fmt.Printf("Checking if `%s` version %s is installed\n", binary, version)
+	found, err := pkg.IsCommandAvailable(binary, cmd, "")
+	if err != nil {
+		return err
+	}
+
+	if !found {
+		fmt.Printf("`%s` not found\n", binary)
+		switch binary {
+		case "goreleaser":
+			return InstallGoReleaser(version)
+		case "ko":
+			return InstallKO(version)
+		}
+	}
+
+	fmt.Printf("`%s` is installed!\n", binary)
+	return nil
+}
+
+// Maybe we can  move this to release-utils
+func InstallKO(version string) error {
+	fmt.Println("Will install `ko`")
+	target := "ko"
+	if runtime.GOOS == "windows" {
+		target = "ko.exe"
+	}
+
+	opts := archive.DownloadArchiveOptions{
+		DownloadOptions: downloads.DownloadOptions{
+			UrlTemplate: "https://github.com/google/ko/releases/download/v{{.VERSION}}/ko_{{.VERSION}}_{{.GOOS}}_{{.GOARCH}}{{.EXT}}",
+			Name:        "ko",
+			Version:     version,
+			OsReplacement: map[string]string{
+				"darwin":  "Darwin",
+				"linux":   "Linux",
+				"windows": "Windows",
+			},
+			ArchReplacement: map[string]string{
+				"amd64": "x86_64",
+			},
+		},
+		ArchiveExtensions: map[string]string{
+			"linux":   ".tar.gz",
+			"darwin":  ".tar.gz",
+			"windows": ".tar.gz",
+		},
+		TargetFileTemplate: target,
+	}
+
+	return archive.DownloadToGopathBin(opts)
+}
+
+// Maybe we can  move this to release-utils
+func InstallGoReleaser(version string) error {
+	fmt.Println("Will install `goreleaser` version `%s`", version)
+	target := "goreleaser"
+	opts := archive.DownloadArchiveOptions{
+		DownloadOptions: downloads.DownloadOptions{
+			// https://github.com/goreleaser/goreleaser/releases/download/v1.10.3/goreleaser_Linux_arm64.tar.gz.sbom
+			UrlTemplate: "https://github.com/goreleaser/goreleaser/releases/download/v{{.VERSION}}/goreleaser_{{.GOOS}}_{{.GOARCH}}{{.EXT}}",
+			Name:        "goreleaser",
+			Version:     version,
+			OsReplacement: map[string]string{
+				"darwin":  "Darwin",
+				"linux":   "Linux",
+				"windows": "Windows",
+			},
+			ArchReplacement: map[string]string{
+				"amd64": "x86_64",
+			},
+		},
+		ArchiveExtensions: map[string]string{
+			"linux":   ".tar.gz",
+			"darwin":  ".tar.gz",
+			"windows": ".tar.gz",
+		},
+		TargetFileTemplate: target,
+	}
+
+	return archive.DownloadToGopathBin(opts)
+}
